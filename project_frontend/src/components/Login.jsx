3be669eee73793a42ca288ddb8a318611b1f30a1
muffin-5 <pratham.agarwal.cse22@iitbhu.ac.in>
2024-11-07 16:26:34 +0530

    some frontend changes

--- project_frontend/src/components/Login.jsx
+++ project_frontend/src/components/Login.jsx
@@ -31,10 +31,7 @@ const Login = () => {
 
       // Handle successful login
       const responseData = response.data; // Assuming the response contains user/counselor ID
-<<<<<<< HEAD
-=======
       localStorage.setItem("token", responseData.token);
->>>>>>> 46737418a34eb6d8b03160ee99bc0af28e9380fe
 
       if (userType === "user") {
         // Assuming responseData contains userId for users
@@ -46,10 +43,7 @@ const Login = () => {
         localStorage.setItem("whoLogged", "counselor");
       }
       localStorage.setItem("isAuthenticated", "true");
-<<<<<<< HEAD
-=======
-      console.log(responseData.token)
->>>>>>> 46737418a34eb6d8b03160ee99bc0af28e9380fe
+      console.log(responseData.token);
 
       // Redirect based on user type
       if (userType === "user") {
